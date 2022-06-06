ef4fd492ff077eefbfe629b3371a685d9a2fcda7
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-06-06 22:15:20 +0200

    buffer reader

--- buffer/src/test/java/com/javastudio/tutorial/BufferReaderTest.java
+++ buffer/src/test/java/com/javastudio/tutorial/BufferReaderTest.java
@@ -1,40 +1,39 @@
 package com.javastudio.tutorial;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
+import java.io.InputStreamReader;
 
+@ExtendWith(MockitoExtension.class)
 class BufferReaderTest {
 
-    public static final String TEMP_FILE_PREFIX = "tmp";
-    public static final String TEMP_FILE_SUFFIX = ".txt";
-
     @Test
-    @Tag("Java6")
-    void writeTextToFileUsingTheClassicFileWriterAndBufferedWriter() throws IOException {
-        File file = File.createTempFile(TEMP_FILE_PREFIX, TEMP_FILE_SUFFIX);
-        System.out.println(file.getAbsolutePath());
-        try (FileWriter fileWriter = new FileWriter(file);
-             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
-            bufferedWriter.write("This is a test");
-            bufferedWriter.newLine();
+    void readTextFileUsingStreamAsUnderLayingSource() throws IOException {
+        String buffer;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                this.getClass().getClassLoader().getResourceAsStream("rfc6238.txt")))) {
+            while ((buffer = reader.readLine()) != null) {
+
+            }
         }
     }
 
     @Test
-    @Tag("Java7")
-    void writeTextToFileUsingNIOClassFiles() throws IOException {
-        Path path = File.createTempFile(TEMP_FILE_PREFIX, TEMP_FILE_SUFFIX).toPath();
-        System.out.println(path.toAbsolutePath());
-        String content = "This is a test";
-        Files.write(path, content.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+    void readTextFileWrappingFileReaderInBufferReader() throws IOException {
+        String buffer;
+
+        // By default, this will use a buffer of 8 KB.
+        try (BufferedReader reader =
+                     new BufferedReader(new FileReader("src/main/resources/rfc6238.txt"), 16384)) {
+            while ((buffer = reader.readLine()) != null) {
+
+            }
+        }
     }
 }
