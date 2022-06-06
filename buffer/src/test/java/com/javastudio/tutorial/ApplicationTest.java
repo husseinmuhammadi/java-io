d660b8aa305ae83691326f3f8af749693a8d2efb
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-06-06 21:37:42 +0200

    buffer reader

--- buffer/src/test/java/com/javastudio/tutorial/ApplicationTest.java
+++ buffer/src/test/java/com/javastudio/tutorial/ApplicationTest.java
@@ -1,5 +1,4 @@
 package com.javastudio.tutorial;
 
 class ApplicationTest {
-
 }
---
+++ buffer/src/test/java/com/javastudio/tutorial/BufferReaderTest.java
@@ -0,0 +1,40 @@
+package com.javastudio.tutorial;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+class BufferReaderTest {
+
+    public static final String TEMP_FILE_PREFIX = "tmp";
+    public static final String TEMP_FILE_SUFFIX = ".txt";
+
+    @Test
+    @Tag("Java6")
+    void writeTextToFileUsingTheClassicFileWriterAndBufferedWriter() throws IOException {
+        File file = File.createTempFile(TEMP_FILE_PREFIX, TEMP_FILE_SUFFIX);
+        System.out.println(file.getAbsolutePath());
+        try (FileWriter fileWriter = new FileWriter(file);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write("This is a test");
+            bufferedWriter.newLine();
+        }
+    }
+
+    @Test
+    @Tag("Java7")
+    void writeTextToFileUsingNIOClassFiles() throws IOException {
+        Path path = File.createTempFile(TEMP_FILE_PREFIX, TEMP_FILE_SUFFIX).toPath();
+        System.out.println(path.toAbsolutePath());
+        String content = "This is a test";
+        Files.write(path, content.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+    }
+}
